d7bf6935ee46c966c92863b50dbfe7f9e686a5ee
Turakulov <akram.turakulov@gmail.com>
2025-03-26 01:43:20 +0300

    Dags and jobs edited

--- synthetic_data_vault/entire_data_producer.py
+++ synthetic_data_vault/entire_data_producer.py
@@ -2,88 +2,108 @@ import json
 import pickle
 import uuid
 import logging
-from confluent_kafka import Producer
+import asyncio
+from aiokafka import AIOKafkaProducer
 from confluent_kafka.admin import AdminClient, NewTopic
 
 # from sdv.datasets.demo import get_available_demos, download_demo
-from confluent_kafka import Consumer, TopicPartition
+
+log = logging.getLogger()
+logging.basicConfig()
 
 CONFIG = {
     "bootstrap.servers": "localhost:9092",
     # 'group.id': ''
 }
+TOPICS = [
+    # "employees",
+    #  "customers",
+    # "products",
+    "sales"
+]
+SEMAPHORE_LIMIT = 10000
 
 
-def delivery_report(err, msg):
+async def delivery_report(err, msg):
     if err is not None:
         print(f"Error with message: {err}")
     else:
         print(
-            f"Message sent to topic: {msg.topic()} ; partition:[{msg.partition()}] ; offset: {msg.offset()}"
+            f"Message sent to topic: {msg.topic} ; partition:[{msg.partition}] ; offset: {msg.offset}"
         )
 
 
-def execute_producer(producer, topic: str, data: str):
-    producer.produce(topic, key=str(uuid.uuid4()), value=data, callback=delivery_report)
-    producer.poll(0)
-    producer.flush()
+async def execute_producer(producer, topic: str, data: str):
+    try:
+        key = str(uuid.uuid4()).encode("utf-8")
+        value = data.encode("utf-8")
+        await producer.send_and_wait(topic, key=key, value=value)
+    except Exception as e:
+        print(f"Failed to send message: {e}")
 
 
-# data, metadata = download_demo(
-#     modality='multi_table',
-#     dataset_name= 'world_v1' #'SalesDB_v1'
-# )
-# print(data, data.keys(), type(data))
-# with open('data.pickle', 'wb') as handle:
-#     pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
-#
-# print(metadata, metadata.keys(), type(metadata))
-# with open('metadata.json', 'w') as handle:
-#     json.dump(metadata, handle)
-
-
-def check_topic_existance(topic_name):
+def check_topic_existence(topic_name):
     # Определить конфигурацию темы
     topic_config = {
         "cleanup.policy": "delete",
         "retention.ms": "2419200000",  # 4 недели
     }
-
     admin_client = AdminClient(CONFIG)
     topic_metadata = admin_client.list_topics()
     if topic_metadata.topics.get(topic_name) is None:
         # Создать экземпляр NewTopic
         new_topic = NewTopic(
-            topic_name, num_partitions=1, replication_factor=1, config=topic_config
+            topic=topic_name,
+            num_partitions=1,
+            replication_factor=1,
+            config=topic_config,
         )
         # Создать топик с помощью AdminClient
         admin_client.create_topics([new_topic])
 
 
-with open("data.pickle", "rb") as handle:
-    data = pickle.load(handle)
+async def produce_data():
+    # data, metadata = download_demo(
+    #     modality='multi_table',
+    #     dataset_name= 'world_v1' #'SalesDB_v1'
+    # )
+    # print(data, data.keys(), type(data))
+    # with open('data.pickle', 'wb') as handle:
+    #     pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    #
+    # print(metadata, metadata.keys(), type(metadata))
+    # with open('metadata.json', 'w') as handle:
+    #     json.dump(metadata, handle)
+
+    with open("data.pickle", "rb") as handle:
+        data = pickle.load(handle)
+
+    for topic in TOPICS:
+        check_topic_existence(topic)
+
+    producer = AIOKafkaProducer(bootstrap_servers=CONFIG["bootstrap.servers"])
+    await producer.start()
+
+    semaphore = asyncio.Semaphore(SEMAPHORE_LIMIT)
+
+    async def limited_produce(topic_name, json_data):
+        async with semaphore:
+            await execute_producer(producer, topic_name, json_data)
+
+    try:
+        tasks = []
+        for t in TOPICS:
+            for i in range(data[t.capitalize()].shape[0]):
+                tasks.append(
+                    limited_produce(
+                        topic_name=t, json_data=data[t.capitalize()].iloc[i].to_json()
+                    )
+                )
+
+        await asyncio.gather(*tasks)
+    finally:
+        await producer.stop()
 
 
-check_topic_existance("employees")
-check_topic_existance("customers")
-check_topic_existance("products")
-check_topic_existance("sales")
-
-producer = Producer(CONFIG)
-for i in range(data["Employees"].shape[0]):
-    execute_producer(
-        producer=producer, topic="employees", data=data["Employees"].iloc[i].to_json()
-    )
-
-for i in range(data["Customers"].shape[0]):
-    execute_producer(
-        producer=producer, topic="customers", data=data["Customers"].iloc[i].to_json()
-    )
-
-for i in range(data["Products"].shape[0]):
-    execute_producer(
-        producer=producer, topic="products", data=data["Products"].iloc[i].to_json()
-    )
-
-# for i in range(data['Sales'].shape[0]):
-#     execute_producer(producer=producer, topic='sales', data=data['Sales'].iloc[i].to_json())
+if __name__ == "__main__":
+    asyncio.run(produce_data())
